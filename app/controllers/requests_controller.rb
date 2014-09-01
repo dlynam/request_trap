b9255ee9377f98b1138702da2d56c6abcc4c0632
David Lynam <dlynam@gmail.com>
2014-09-01 15:55:41 -0700

    add request partial

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,21 +1,25 @@
 class RequestsController < ApplicationController
-  helper_method :trap_id
+  helper_method :trap_id, :requests
 
   def create
     CreateRequest.new(trap_id, request, response).create
   end
 
   def index
-    @requests = Request.where trap_id: trap_id
   end
 
   def show
+    @request = requests.find params[:id]
   end
 
   private
 
   def trap_id
     @trap_id ||= params[:trap_id]
+  end
+
+  def requests
+    @requests ||= Request.where trap_id: trap_id
   end
 
 end
