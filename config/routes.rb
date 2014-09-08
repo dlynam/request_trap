d05314e84db2b07ddcc1bf4e43c920db0bf06981
David Lynam <dlynam@gmail.com>
2014-09-07 18:33:11 -0700

    remove stale monit_requests route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  #get 'monitor_requests', to: 'monitor_requests#index'
   resources :monitor_requests, only: [ :show ]
   match ':trap_id', to: 'requests#create', via: [:get, :post, :put, :delete]
   get ':trap_id/requests', to: 'requests#index'
